b1e488a6f9c4a3e14b96db5800f281f0af81ca82
Jihui Huang <717655909@qq.com>
2020-03-08 17:21:36 +0800

    add circle part

--- src/main.cpp
+++ src/main.cpp
@@ -16,6 +16,37 @@ using namespace std;
 
 
 
+void Intersect::addCmd(string text)
+{
+	string buf1, buf2, buf3, buf4, buf5;
+	int x1, x2, y1, y2, r;
+	stringstream s(text);
+	s >> buf1 >> buf2 >> buf3 >> buf4 >> buf5;
+	if (buf1[0] == 'L') {
+		x1 = stoi(buf2);
+		y1 = stoi(buf3);
+		x2 = stoi(buf4);
+		y2 = stoi(buf5);
+		this->addLine(x1, y1, x2, y2);
+	}
+	else if (buf1[0] == 'C') {
+		x1 = stoi(buf2);
+		y1 = stoi(buf3);
+		r = stoi(buf4);
+		this->addCircle(x1, y1, r);
+	}
+	
+}
+
+void Intersect::addCircle(int x, int y, int r)
+{
+	Circle *circle = new Circle();
+	circle->x = x;
+	circle->y = y;
+	circle->r = r;
+	circles.push_back(*circle);
+}
+
 void Intersect::addLine(int x1, int y1, int x2, int y2)
 {
 	Line *line = new Line();
@@ -71,22 +102,74 @@ void Intersect::intersect2lines(Line line1, Line line2)
 	return;
 }
 
-void Intersect::addIntersectPoint(IntersectPoint intersectpoint)
+void Intersect::intersect2circles(Circle circle1, Circle circle2)
 {
-	/*
-	unsigned int k;
-	for (k = 0; k < intersectpoints.size(); k++) {
-		IntersectPoint ip2 = intersectpoints[k];
-		if (ip2.x == intersectpoint.x && ip2.y == intersectpoint.y) {
-			break;
+	double x1 = (double)circle1.x;
+	double y1 = (double)circle1.y;
+	double x2 = (double)circle2.x;
+	double y2 = (double)circle2.y;
+	double r1 = (double)circle1.r;
+	double r2 = (double)circle2.r;
+	double k = (x2 - x1) / (y2 - y1);
+	double b = (((r1 * r1 - r2 * r2) - (-x1 - x2) *(x2 - x1)) / (y2 - y1) + y1 + y2) / 2;
+	Line l;
+	l.k = k;
+	l.b = b;
+	l.x1 = (((r1 * r1 - r2 * r2) - (y1 + y2) *(y2 - y1)) / (x1 - x2) + x1 + x2) / 2;
+	l.x2 = l.x1;
+	intersectLine2circle(l, circle1);
+}
+
+void Intersect::intersectLine2circle(Line line, Circle circle)
+{
+	double k = line.k;
+	double b = line.b;
+	double x1 = circle.x;
+	double y1 = circle.y;
+	double r= circle.r;
+	if (isinf(k)) {
+		double ans_x1 = line.x1;
+		double d = r * r - (ans_x1 - x1) * (ans_x1 - x1);
+		if (d >= 0) {
+			double ans_y1 = y1 + sqrt(d);
+			double ans_y2 = y1 - sqrt(d);
+			IntersectPoint i1;
+			IntersectPoint i2;
+			i1.x = ans_x1;
+			i1.y = ans_y1;
+			i2.x = ans_x1;
+			i2.y = ans_y2;
+			addIntersectPoint(i1);
+			addIntersectPoint(i2);
 		}
 	}
-	if (k == intersectpoints.size()) {
-		intersectpoints.push_back(intersectpoint);
+	else {
+		double a = 1 + k * k;
+		double b2 = -2 * x1 + 2 * k * b;
+		double c = x1 * x1 + b * b + y1 * y1 - 2 * k * y1 - 2 * b * y1 - r * r;
+		double d = b * b - 4 * a * c;
+		if (d >= 0) {
+			double ans_x1 = (-b2 + sqrt(d)) / (2 * a);
+			double ans_y1 = ans_x1 * k + b;
+			double ans_x2 = (-b2 - sqrt(d)) / (2 * a);
+			double ans_y2 = ans_x2 * k + b;
+			IntersectPoint i1;
+			IntersectPoint i2;
+			i1.x = ans_x1;
+			i1.y = ans_y1;
+			i2.x = ans_x2;
+			i2.y = ans_y2;
+			addIntersectPoint(i1);
+			addIntersectPoint(i2);
+		}
 	}
-	*/
+	
+}
+
+void Intersect::addIntersectPoint(IntersectPoint intersectpoint)
+{
+
 	intersectpoints.insert(pair<double,double>(intersectpoint.x, intersectpoint.y));
-	//intersectpoints->insert({ pair<double, double>(intersectpoint.x, intersectpoint.y), intersectpoint });
 }
 
 int Intersect::intersect()
@@ -99,6 +182,16 @@ int Intersect::intersect()
 			intersect2lines(lines[i], lines[j]);
 		}
 	}
+	for (unsigned int i = 0; i < circles.size() - 1; i++) {
+		for (unsigned int j = i + 1; j < circles.size(); j++) {
+			intersect2circles(circles[i], circles[j]);
+		}
+	}
+	for (unsigned int i = 0; i < lines.size(); i++) {
+		for (unsigned int j = 0; j < circles.size(); j++) {
+			intersectLine2circle(lines[i], circles[j]);
+		}
+	}
 	return intersectpoints.size();
 }
 
@@ -107,8 +200,6 @@ int main(int argc, char **argv)
 	int num;
 	Intersect intersect;
 	int test = 0;
-	int x1, x2, y1, y2;
-	char c;
 	if (test) {
 		intersect.addLine(0, 0, 0, 1);
 		num = 5000;
@@ -119,15 +210,13 @@ int main(int argc, char **argv)
 		return 0;
 	}
 	if (argc < 2) {
-		cin >> num;
+		cin >> num ;
+		while (cin.get() != '\n');
 		for (int i = 0; i < num; i++) {
-				
-			cin >> c;
-			cin >> x1;
-			cin >> y1;
-			cin >> x2;
-			cin >> y2;
-			intersect.addLine(x1, y2, x2, y2);
+			char buffer[256];
+			fgets(buffer, 256, stdin);
+			string cmd(buffer);
+			intersect.addCmd(cmd);
 		}
 		cout << intersect.intersect();
 	}
@@ -144,14 +233,7 @@ int main(int argc, char **argv)
 		num = stoi(text);
 		for (int i = 0; i < num; i++) {
 			getline(in, text);
-			string buf1, buf2, buf3, buf4, buf5;
-			stringstream s(text);
-			s >> buf1 >> buf2 >> buf3 >> buf4 >> buf5;
-			x1 = stoi(buf2);
-			y1 = stoi(buf3);
-			x2 = stoi(buf4);
-			y2 = stoi(buf5);
-			intersect.addLine(x1, y1, x2, y2);
+			intersect.addCmd(text);
 		}
 		//cout << intersect.intersect();
 		out << intersect.intersect();
--- src/main.h
+++ src/main.h
@@ -8,10 +8,10 @@ using namespace std;
 
 class Line {
 public:
-	int x1;
-	int x2;
-	int y1;
-	int y2;
+	double x1;
+	double x2;
+	double y1;
+	double y2;
 	double k;
 	double b;
 };
@@ -22,15 +22,27 @@ public:
 	double y = 0;
 };
 
+class Circle {
+public:
+	int r;
+	int x;
+	int y;
+};
+
 class Intersect {
 private:
 	vector<Line> lines;
+	vector<Circle> circles;
 	//unordered_map<pair<double, double>, IntersectPoint> *intersectpoints = new unordered_map<pair<double, double>, IntersectPoint>();
 	set<pair<double, double>> intersectpoints;
 
 public:
+	void addCmd(string s);
+	void addCircle(int x, int y, int r);
 	void addLine(int x1, int y1, int x2, int y2);
 	void intersect2lines(Line line1, Line line2);
+	void intersect2circles(Circle circle1, Circle circle2);
+	void intersectLine2circle(Line line, Circle circle);
 	void addIntersectPoint(IntersectPoint intersectpoint);
 	int intersect();
 };
